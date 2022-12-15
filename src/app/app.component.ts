8159d06488f80f1a137a2e635eece7a6778cdc23
Jonatan Tiago <jonatan2206@gmail.com>
2022-12-15 16:03:33 -0300

    Enable inputs progressively

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { formatNumber } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit {
   codeToCopy = '';
   codeForm!: FormGroup;
 
+  @ViewChildren('digitInputs') digitInputs!: QueryList<ElementRef<HTMLInputElement>>;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -23,14 +25,42 @@ export class AppComponent implements OnInit {
     ];
 
     this.codeForm = this.formBuilder.group({
+      digit0: new FormControl('', digitValidator),
       digit1: new FormControl('', digitValidator),
       digit2: new FormControl('', digitValidator),
       digit3: new FormControl('', digitValidator),
       digit4: new FormControl('', digitValidator),
-      digit5: new FormControl('', digitValidator),
-      digit6: new FormControl('', digitValidator)
+      digit5: new FormControl('', digitValidator)
     });
+    this.enableOrDisableInputs();
 
-    this.codeToCopy = `${Math.floor(Math.random() * 1E+6)}`.padStart(6, '0');
+    this.codeToCopy = `${Math.floor(Math.random() * 1e+6)}`.padStart(6, '0');
+  }
+
+  focusNext(event: Event, index: number): void {
+    this.enableOrDisableInputs();
+    if ((event.target as HTMLInputElement).value) {
+      this.digitInputs.get(index + 1)?.nativeElement.focus();
+    }
+  }
+
+  fillFromClipboard(event: ClipboardEvent): void {
+    const pastedText = event.clipboardData?.getData('text');
+    if (!pastedText || !/\d{1,6}/.test(pastedText)) {
+      return;
+    }
+  }
+
+  private enableOrDisableInputs(): void {
+    Array.from({ length: 5 }, (_, i) => i + 1).forEach(i => {
+      const current = this.codeForm.get(`digit${i}`);
+      const previous = this.codeForm.get(`digit${i - 1}`);
+      if (previous?.value) {
+        current?.enable();
+      } else {
+        current?.setValue('');
+        current?.disable();
+      }
+    });
   }
 }
