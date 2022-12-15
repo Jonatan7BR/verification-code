e35b45317078356a1ab853fd2c48ffeb2138f8ca
Jonatan Tiago <jonatan2206@gmail.com>
2022-12-15 14:46:13 -0300

    Page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { formatNumber } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'verification-code';
+
+  codeToCopy = '';
+  codeForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    const digitValidator = [
+      Validators.required,
+      Validators.maxLength(1),
+      Validators.pattern(/\d/)
+    ];
+
+    this.codeForm = this.formBuilder.group({
+      digit1: new FormControl('', digitValidator),
+      digit2: new FormControl('', digitValidator),
+      digit3: new FormControl('', digitValidator),
+      digit4: new FormControl('', digitValidator),
+      digit5: new FormControl('', digitValidator),
+      digit6: new FormControl('', digitValidator)
+    });
+
+    this.codeToCopy = `${Math.floor(Math.random() * 1E+6)}`.padStart(6, '0');
+  }
 }
